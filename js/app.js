44ffa79830e12907faefc76ae7c309d335dff19e
anJu0703 <jpmantilla2000@hotmail.com>
2024-02-17 11:43:47 -0500

    Agregar inputs a obj

---
+++ js/app.js
@@ -0,0 +1,38 @@
+//Variables documento
+//Inputs
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+//UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+//Variables
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+};
+
+//eventListeners
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+};
+
+//Funciones
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+};
