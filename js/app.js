2642f340556892f989a11624a51ebca690c5745c
anJu0703 <jpmantilla2000@hotmail.com>
2024-02-18 21:26:57 -0500

    Agregar btn eliminar

--- js/app.js
+++ js/app.js
@@ -31,6 +31,10 @@ class Citas{
         this.citas = [...this.citas, cita];
 
     };
+
+    eliminarCita(id){
+        this.citas = this.citas.filter(cita => cita.id !== id)
+    };
 };
 
 class UI{
@@ -101,6 +105,16 @@ class UI{
                 <span class="font-weight-bolder">Sintomas: </span>  ${sintomas}
             `;
 
+            //Boton para eliminar esta cita
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = `Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+          `;
+
+          btnEliminar.onclick = ()=> eliminarCita(id);
+
             //Agregar los parrafos al Divcita
             divCita.appendChild(mascotaParrafo);
             divCita.appendChild(propietarioParrafo);
@@ -108,6 +122,7 @@ class UI{
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
 
             //Agregar las citas al HTML
             contenedorCitas.appendChild(divCita);
@@ -181,4 +196,15 @@ function reiniciarObjeto(){
     citaObj.fecha = '';
     citaObj.hora = '';
     citaObj.sintomas = '';
+};
+
+function eliminarCita(id) {
+    //Eliminar la cita
+    administrarCitas.eliminarCita(id);
+
+    //Muestre mensaje
+    ui.imprimirAlerta('La cita se elimino correctamente');
+
+    //Refrescar la cita
+    ui.imprimirCitas(administrarCitas);
 };
